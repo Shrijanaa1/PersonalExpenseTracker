66a2fbe6ce16996f5dc963b27e5006712226f78e
Shrijana Karki <srznakrki111@gmail.com>
2024-08-11 20:55:43 +0545

    Edited UserBean and Created userList page for listing and deleting user data

--- src/main/java/com/syntech/pem/bean/UserBean.java
+++ src/main/java/com/syntech/pem/bean/UserBean.java
@@ -5,9 +5,10 @@ import com.syntech.pem.service.UserService;
 import java.io.Serializable;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
-
 
 @Named
 @RequestScoped
@@ -17,45 +18,82 @@ public class UserBean implements Serializable {
     private UserService userService;
     
     private User user = new User();
-    private Long userId;
+
+    private String username;
+    private String password;
+
     
-    public void createUser(){
-        userService.save(user);
-    }
-    
-    public User getUser() {
-        if (userId != null) {
-            user = userService.getById(userId);
+    public String createUser() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (userService.getByUsername(username) != null) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Username taken", "Please choose a different username."));
+            return "signUp"; // Return to the same signup page if username is taken
         }
-        return user;
+
+        user.setUsername(username);
+        user.setPassword(password);
+        userService.save(user);
+        user = new User(); // Clear the form after saving
+        context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "User created successfully."));
+        return "login?faces-redirect=true"; // Redirect to login page after successful signup
     }
 
+    
+    
     public void updateUser() {
         userService.update(user);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "User updated successfully."));
     }
     
-    public void deleteUser() {
-        userService.delete(userId);
+    
+    public void deleteUser(User user) {
+        userService.delete(user.getId());
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "User deleted successfully."));
     }
 
+    
     public List<User> getAllUsers() {
         return userService.getAll();
     }
     
     
+    public String login() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        User user = userService.getByUsername(username);
+
+        if (user != null && user.getPassword().equals(password)) { // Ideally,compare passwords
+            context.getExternalContext().getSessionMap().put("user", user);
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Success", "Login successful"));
+            return "userDashboard?faces-redirect=true";
+        } else {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid credentials", "Please try again."));
+            return "login";
+        }
+    }
+    
     // Getters and Setters
     
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
+    public User getUserInstance() {
+        return user;
     }
 
     public void setUser(User user) {
         this.user = user;
     }
 
-    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
