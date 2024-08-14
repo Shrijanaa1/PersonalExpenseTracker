2c11db0db2327ebfabb3fd4407ff9ff5f165a44a
Shrijana Karki <srznakrki111@gmail.com>
2024-08-14 14:56:44 +0545

    Created CRUD operation for Transaction

---
+++ src/main/java/com/syntech/pem/repository/TransactionRepository.java
@@ -0,0 +1,20 @@
+package com.syntech.pem.repository;
+
+import com.syntech.pem.model.Transaction;
+import java.util.List;
+import javax.persistence.TypedQuery;
+
+public class TransactionRepository extends GenericRepository<Transaction>{
+    
+    @Override
+    public Transaction findById(Long id){
+        return getEntityManager().find(Transaction.class, id);
+    }
+    
+    @Override
+    public List<Transaction> findAll(){
+        TypedQuery<Transaction> query = getEntityManager().createQuery("SELECT  t FROM Transaction t", Transaction.class);
+        return query.getResultList();
+   } 
+    
+}
