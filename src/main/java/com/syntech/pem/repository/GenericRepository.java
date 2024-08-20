b1ed5a97ac9781fc606fcb3ed86e463ef74f4c2e
Shrijana Karki <srznakrki111@gmail.com>
2024-08-20 13:00:28 +0545

    Updated GenericRepository to add Criteria Query

--- src/main/java/com/syntech/pem/repository/GenericRepository.java
+++ src/main/java/com/syntech/pem/repository/GenericRepository.java
@@ -4,6 +4,10 @@ import com.syntech.pem.model.BaseIdEntity;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
 public abstract class GenericRepository<T extends BaseIdEntity> {
@@ -34,8 +38,35 @@ public abstract class GenericRepository<T extends BaseIdEntity> {
         }
         entityManager.remove(entity);
     }
-
-    // Getter and setter for entityManager for subclasses to use
+    
+    
+    //Criteria Queries
+    
+    protected List<T> findByCriteria(CriteriaQuery<T> criteriaQuery){
+        return entityManager.createQuery(criteriaQuery).getResultList();
+    }
+    
+    //Generic method to create a criteria query by a single attribute
+    protected List<T> findByAttribute(String attributeName, Object attributeValue, Class<T> entityClass){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(entityClass);
+        Root<T> entityRoot = criteriaQuery.from(entityClass);
+        
+        Predicate predicate = criteriaBuilder.equal(entityRoot.get(attributeName), attributeValue);
+        criteriaQuery.select(entityRoot).where(predicate);
+        
+        return findByCriteria(criteriaQuery);
+    }
+    
+    protected List<T> findAll(Class<T> entityClass){
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<T> criteriaQuery = criteriaBuilder.createQuery(entityClass);
+        Root<T> entityRoot = criteriaQuery.from(entityClass);
+       
+        criteriaQuery.select(entityRoot);
+        
+        return findByCriteria(criteriaQuery);
+    }
     
     protected EntityManager getEntityManager() {
         return entityManager;
@@ -47,7 +78,3 @@ public abstract class GenericRepository<T extends BaseIdEntity> {
     
     
 }
-
-//genric bata create and update crud operations kaile bhako 
-//localdatetime
-//store in database
--- src/main/java/com/syntech/pem/repository/UserRepository.java
+++ src/main/java/com/syntech/pem/repository/UserRepository.java
@@ -3,8 +3,7 @@ package com.syntech.pem.repository;
 import com.syntech.pem.model.User;
 import java.util.List;
 import javax.ejb.Stateless;
-import javax.persistence.NoResultException;
-import javax.persistence.TypedQuery;
+
 
 @Stateless
 public class UserRepository extends GenericRepository<User>{
@@ -14,21 +13,25 @@ public class UserRepository extends GenericRepository<User>{
         return getEntityManager().find(User.class, id);
     }
     
+    
     @Override
     public List<User> findAll(){
-        TypedQuery<User> query = getEntityManager().createQuery("SELECT  u FROM User u", User.class);
-        return query.getResultList();
-   } 
+        return findAll(User.class);
+    } 
     
-
     public User getByUsername(String username) {
-            try {
-                return getEntityManager().createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
-                        .setParameter("username", username)
-                        .getSingleResult();
-            } catch (NoResultException e) {
-                return null;
-            }
-        }
+        List<User> users = findByAttribute("username", username, User.class);
+        return users.isEmpty() ? null : users.get(0);
+    }
+
+//    public User getByUsername(String username) {
+//            try {
+//                return getEntityManager().createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
+//                        .setParameter("username", username)
+//                        .getSingleResult();
+//            } catch (NoResultException e) {
+//                return null;
+//            }
+//        }
 
 }
