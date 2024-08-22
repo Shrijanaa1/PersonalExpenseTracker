08b95f980774367435f910b37bd57612e342eaad
Shrijana Karki <srznakrki111@gmail.com>
2024-08-22 12:20:34 +0545

    Removed Overridden methods of LazyDataModel from AccoutRepository

--- src/main/java/com/syntech/pem/repository/AccountRepository.java
+++ src/main/java/com/syntech/pem/repository/AccountRepository.java
@@ -2,11 +2,9 @@ package com.syntech.pem.repository;
 
 import com.syntech.pem.model.Account;
 import java.util.List;
-import java.util.Map;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import org.primefaces.model.FilterMeta;
 
 @Stateless
 public class AccountRepository extends GenericRepository<Account> {
@@ -37,20 +35,5 @@ public class AccountRepository extends GenericRepository<Account> {
     
     public Account findAccountByName(String name){
         return findByAttribute("name", name).stream().findFirst().orElse(null);
-    }
-    
-    
-    public List<Account> findRange(int first, int pageSize) {
-        String query = "SELECT a FROM Account a";
-        return entityManager.createQuery(query, Account.class)
-                .setFirstResult(first)
-                .setMaxResults(pageSize)
-                .getResultList();
-                
-    }
-    
-    public int count(Map<String, FilterMeta> filterBy) {
-        String query = "SELECT COUNT(a) FROM Account a";
-        return ((Long) entityManager.createQuery(query).getSingleResult()).intValue();
     }
 }
